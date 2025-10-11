05d9a5cadf68925b9e0bfab513fa3feac8ccd148
Sadiya Mahamoud <sacdamamoud@gmail.com>
2025-10-12 00:58:40 +0300

    Update DashoardCards.jsx

--- src/components/DashoardCards.jsx
+++ src/components/DashoardCards.jsx
@@ -1,6 +1,6 @@
 import { BsBook, BsPerson } from "react-icons/bs";
 import { useAuth } from "../context/AuthContext";
-import { useCourses } from "../context/CoursesContext";
+import { useCourses } from "../context/coursesContext";
 import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabaseClient";
 
